43a56957b4995f7decd1a5b24969ed876b53286a
Egor Babin <babin.e.k@effective-group.ru>
2023-09-25 14:48:32 +0300

    IPR

---
+++ M4/1.1 Навыки и компетенции/1.1.5 Регулярные выражения/1.1.5.1 Поиск в исходном коде по регулярным выражениям/index.php
@@ -0,0 +1,35 @@
+номер терефона
+(\+?[0-9]?\s?\(?[0-9]{3}\)?\s?[0-9]{3}.?[0-9]{2,3}.?[0-9]{2,3})
+
+номер телефона вида +7 123 123 12 12 или +71231231212
+(\+[0-9]{1}\s?[0-9]{3}\s?[0-9]{3}\s?[0-9]{2}\s?[0-9]{2})
+
+фалы экшенов отмены чего-либо, содержащие функцию processForm
+(class\sAction\w*Giveup[\w|\W]*processForm[\w|\W]*)
+
+
+(\$this->_?signAllButton\(\s?\$this->_?document[s]?\,?\s?\))
+
+все вьюшки содержащие функцию initScripts или initButtonScripts в которых есть строка "crypto_sign.js"
+([\w|\W]*class[\w|\W]*_View[\w|\W]*_?init[\w|\W]{0,15}Scripts[\w|\W]crypto_sign\.js)
+
+
+поиск строк таких как
+getAuction()->getType() == '360'
+getAuction()->getType() == "360"
+$auction()->getType() == "360"
+auction()->type() == "360"
+$auction()->Type() == "360"
+$auction->getType() != '360'
+$this->_Auction->getType() == '360'
+
+((get)?[a|A]uction(\(\))?->(getType\(\)|type\(\))\s?!?={1,2}\s?('|")360)
+
+
+(_?tariff_?(c|C)ontroller.*new.*(S|s)endProposal)
+
+(class.*Dev[\w\W]*('|")(Q|q)uestionn?aireUR)
+
+(if\s\(\$((C|c)an)?(c|C)hange_?(p|P)roposal[\W|\w]*{[\w|\W]*(D|d)elete_?(P|p)roposal_?(p|P)ath\([\w|\W]*\)[\w|\W]*})
+
+(afterDelete[\w|\W]*)\{[\w|\W]*(?!Proposal_Helper_UniqueFile)[\w|\W]*\} - все afterDelete без Proposal_Helper_UniqueFile внутри
